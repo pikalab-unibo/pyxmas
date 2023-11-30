448ebb943d79e0ea89a1ae742388d147a705ecb0
Emre Kuru <emre.kuru@ozu.edu.tr>
2023-11-30 23:13:53 +0300

    fix:Added a better waiting mechanism for the feedback. We can get user feedback as in both accept and reject with accompaniying feedback

--- Expectation/UI_message_handler.py
+++ Expectation/UI_message_handler.py
@@ -1,18 +1,16 @@
-from fastapi import FastAPI, HTTPException, Request
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Request, status
 import requests
 import json
 from fastapi.middleware.cors import CORSMiddleware
+import asyncio
 
 app = FastAPI()
 
 feedback_user_map = dict()
-
-class UserResponse(BaseModel):
-    uuid: str
-    obj: dict
-    obj_name: str
-    obj_id: float
+feedback_lock = asyncio.Lock()
+accepted = False
+accepted_lock = asyncio.Lock()
+initial = True
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,46 +20,78 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.post("/save-user-response")
+@app.post("/explanation-generation/get-recipe")
 async def save_user_response(request: Request):
-    # Parse the request body to Python dict
+    global feedback_user_map, initial
+
     request_data = await request.json()
+    if initial:
+        initial = False
 
-    # Validate data using Pydantic model
-    try:
-        user_response = UserResponse(**request_data)
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Invalid data: {e}")
+    else:
+        async with feedback_lock:
+            feedback_user_map[request_data["uuid"]] = request_data["feedback"]
 
-    feedback_user_map[request_data["uuid"]] = request_data
-
-    url = "http://127.0.0.1:8000/active-learning/save-user-response"
-
-    # Serialize the Pydantic model to JSON
+    url = "http://127.0.0.1:8000/explanation-generation/get-recipe"
     serialized_data = json.dumps(request_data)
 
-    # Send the POST request to the internal URL
     try:
         internal_response = requests.post(url, data=serialized_data, headers={"Content-Type": "application/json"})
         internal_response.raise_for_status()
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
 
-    return {"status": "success", "response": internal_response.json()}
+    return internal_response.json()
 
 
-async def get_feedback(user_id):
+@app.post("/explanation-generation/end-negotiation")
+async def save_user_response(request: Request):
 
-    global feedback_user_map
+    global accepted
 
-    if user_id in feedback_user_map:
-        result = feedback_user_map[user_id]
-        feedback_user_map.pop(user_id,None)
-        print(result)
-        return result
+    print("Accepted!")
+
+    async with accepted_lock:
+        accepted = True
+
+    request_data = await request.json()
+    serialized_data = json.dumps(request_data)
+    url = " http://127.0.0.1:8000/explanation-generation/end-negotiation"
+
+    try:
+        internal_response = requests.post(url, data=serialized_data)
+        internal_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+
+    return internal_response.json()
+
+@app.get("/get-feedback/{user_id}")
+async def get_feedback(user_id: str):
+
+    global feedback_user_map, accepted
+
+    if accepted:
+        return {"accepted": True, "feedback": ""}
+
+    timeout = 30  # Timeout in seconds
+    end_time = asyncio.get_event_loop().time() + timeout
+
+    while asyncio.get_event_loop().time() < end_time:
+
+        async with accepted_lock:
+                if accepted:
+                    return {"accepted": True, "feedback": ""}
     
-    else:
-        while user_id  not in feedback_user_map:
-            ""
+        async with feedback_lock:
+            if user_id in feedback_user_map:
+                result = feedback_user_map.pop(user_id)
+                return {"accepted": False, "feedback": result}
+        
+        await asyncio.sleep(2)  # Sleep for 2 seconds before checking again
+
+    raise HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT,
+                        detail="Feedback request timed out")
+
 
             
