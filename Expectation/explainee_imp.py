e38317e0c0be098a38e9e2bb81a973b82722293f
Emre Kuru <emre.kuru@ozu.edu.tr>
2023-11-30 23:14:35 +0300

    feat: get_feedback api call has been added to get user response

--- Expectation/explainee_imp.py
+++ Expectation/explainee_imp.py
@@ -1,11 +1,7 @@
-from typing import Any, Coroutine
 import pyxmas
 from pyxmas.protocol import messages
-import spade
-import spade.agent
-import spade.behaviour
-
-
+from .UI_message_handler import get_feedback
+import requests
 from pyxmas.protocol.explainee import ExplaineeBehaviour, _get_all_state_classes
 import pyxmas.protocol.data as data
 
@@ -13,14 +9,19 @@ import pyxmas.protocol.data as data
 class ExplaineeAgent(pyxmas.Agent):
 
     def __init__(self, jid: str, password: str, verify_security: bool = False):
-        super().__init__(jid, password, verify_security)
-                
+        super().__init__(jid, password, verify_security)      
+        
 
     class EXP_Behaviour(ExplaineeBehaviour):
             
-            def __init__(self, query: data.Query, recipient: str, thread: str = None, impl: data.Types = None):
+            def __init__(self, query: data.Query, recipient: str, thread: str = None, impl: data.Types = None, baseIP = "http://127.0.0.1:8000"):
                 super().__init__(query, recipient, thread, impl)
-
+                self.baseIP = baseIP
+                self.activeLearningAPI = baseIP + "/active-learning"
+                self.explanationAPI = baseIP + "/explanation-generation"
+                self.recommendationAPI = baseIP + "/recommendation"
+                self.user_id = "34"  
+                
 
             async def on_error(self, error: Exception) -> None:
                 "Implement This Methos"
@@ -36,18 +37,6 @@ class ExplaineeAgent(pyxmas.Agent):
                 if answer == 0:
                     return messages.WhyMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
                 
-                elif answer == 1:
-                    return messages.WhyNotMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
-                
-                elif answer == 2:
-                    return messages.AcceptMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
-                
-                elif answer == 3:
-                    return messages.CollisionMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
-                
-                elif answer == 4:
-                    return messages.DisapproveMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
-                
 
             async def handle_details(self,
                                     message: messages.MoreDetailsMessage
@@ -55,21 +44,35 @@ class ExplaineeAgent(pyxmas.Agent):
                 "Implement This Methos"
 
                 #Place hodler for later implementation
-                answer = 2
-                
+
+                url = f"http://127.0.0.1:1234/get-feedback/{self.user_id}"
+                answer = ""
+                try:
+                    response = requests.get(url)
+                    response.raise_for_status()
+                    answer = response.json()
+                except requests.RequestException as e:
+                    print(f"An error occurred: {e}")
+            
+                print(answer)
+            
+                accepted = answer["accepted"]
+
+                feedback = answer["feedback"]
+
                 if answer == 0:
                     return messages.WhyMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
                 
                 elif answer == 1:
                     return messages.WhyNotMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
                 
-                elif answer == 2:
+                elif accepted:
                     return messages.AcceptMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
                 
                 elif answer == 3:
                     return messages.CollisionMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
                 
-                elif answer == 4:
+                elif not accepted:
                     return messages.DisapproveMessage.create(query=message.query,recommendation=message.recommendation,to=self.recipient)
                 
 
@@ -84,7 +87,7 @@ class ExplaineeAgent(pyxmas.Agent):
                 "Implement This Methos"
 
     async def setup(self):
-        query = data.Query(user_id="TestPerson")
+        query = data.Query(user_id= "34")
         self.add_behaviour(self.EXP_Behaviour(query=query, recipient="recommender@localhost"))    
 
 
